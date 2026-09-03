df326a1dbecda524abc5c1a19fa9fd316efbe6d1
nivakclt <nivakclt@gmail.com>
2026-09-03 17:34:16 +0530

    book

--- src/User/pages/Profile.jsx
+++ src/User/pages/Profile.jsx
@@ -9,16 +9,25 @@ function Profile() {
 
   const[sidebar,setSidebar]=useState(false)
 
+  const [sellStatus,setSellStatus]=useState(true)
+  const [bookStatus,setBookStatus]=useState(false)
+  const [purchaseStatus,setPurchaseStatus]=useState(false)
+
+  const trueClass='p-3 border-t-1 border-s-1 border-e-1'
+  const falseClass='p-3 border-b-1'
+
   return (
     <>
     <div className='relative'>
     <Header/>
+
     {/* profile */}
     {
       sidebar &&
-      <Profileupdate/>
+      <Profileupdate setSidebar={setSidebar}/>
 
     }
+
     <div className='w-full'>
       <div className='h-[30vh] bg-gray-900 relative'>
         <div className='absolute -bottom-20 left-10'>
@@ -50,9 +59,82 @@ function Profile() {
         </div>
       </div>
     </div>
-    
     <div>
+    </div>
+
+    <div className='flex justify-center'>
+      <div className='gap-2 mb-50'>
+      <button className={sellStatus?trueClass:falseClass} onClick={() => 
+        {
+          setSellStatus(true)
+          setBookStatus(false)
+          setPurchaseStatus(false)
+
+        }}>
+        Sell Books
+      </button>
+       <button className={bookStatus?trueClass:falseClass} onClick={() => 
+        {
+          setSellStatus(false)
+          setBookStatus(true)
+          setPurchaseStatus(false)
+
+        }}>
+       Book Status
+      </button>
+      <button className={purchaseStatus?trueClass:falseClass} onClick={() => 
+        {
+          setSellStatus(false)
+          setBookStatus(false)
+          setPurchaseStatus(true)
+
+        }}>
+        Purchase History
+      </button>
+      {/* Main Content */}
       
+      {
+        sellStatus &&
+        <div className='bg-gray-200'>
+          <h1 className='text-2xl text-center my-2 py-3 flex justify-center'>Book Details</h1>
+          <div className='grid sm:grid-cols-1 md:grid-cols-2'>
+            <div className='px-2'>
+            <input type='text' placeholder='Title' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+             <input type='text' placeholder='Author' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+            <input type='text' placeholder='No. of Pages' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+           <input type='text' placeholder='Image Url' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+           <input type='text' placeholder='Price' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+           <input type='text' placeholder='Discount Price' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+           <textarea name='' placeholder='Abstract' rows={'8'} id='' className='w-full p-2 bg-white rounded-sm mb-2'></textarea>
+            </div>
+            <div className='px-2'>
+              <input type='text' placeholder='Publisher' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+              <input type='text' placeholder='Language' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+              <input type='text' placeholder='ISBN' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+              <input type='text' placeholder='Category' name='' id='' className='w-full bg-white rounded-sm mb-2'/>
+              <label htmlFor='bookimgfile' className='flex justify-center'>
+                <input type='file' name='' className='hidden' id='bookimgfile'/>
+                <img src='https://png.pngtree.com/png-clipart/20190921/original/pngtree-file-upload-icon-png-image_4717174.jpg' alt='bookimg' className='w-[50%]'/>
+
+                </label>
+                <div className='flex justify-between'>
+                  <button className='bg-green-500 p-2 rounded-sm hover:bg-amber-800 text-white'>RESET</button>
+                  <button className='bg-green-500 p-2 rounded-sm hover:bg-amber-800 text-white'>SUBMIT </button>
+                  </div>
+
+            </div>
+          </div>
+        </div>
+      }
+      {
+        bookStatus &&
+        <div>Book Status</div>
+      }
+      {
+        purchaseStatus &&
+        <div>Purchase History</div>
+      }
+      </div>
     </div>
     </div>
     <Footer/>
--- src/User/pages/components/Profileupdate.jsx
+++ src/User/pages/components/Profileupdate.jsx
@@ -1,10 +1,34 @@
-function Profileupdate() {
+function Profileupdate({setSidebar}) {
   return (
-    <div className="fixed inset-0 z-50 bg-[rgba(0,0,0,0.5)] flex items-center justify-center">
-      <div className="w-[50vw] h-[80vh] bg-white rounded-lg p-4">
-        Profile Update
+    <>
+    <div className="fixed h-screen w-screen top-0 z-1 bg-[rgba(0,0,0,0.5)]">
+      <div className="w-[50vw] h-screen bg-white border-2 rounded-lg p-4">
+        <div className="bg-black text-white py-7 px-5 rounded-t-lg font-bold justify-between flex">
+          <h1>Edit Profile</h1>
+          <button onClick={()=>{setSidebar(false)}}>X</button>
+        </div>
+        <div className="w-full px-5">
+        <label htmlFor="fileinp" className="flex justify-center my-4 cursor-pointer">
+          <input type="file" id="fileinp" className="hidden" />
+          <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuYRCrIZn18jySQOEQ2C2_m-hetlO7LY0vHYXdd6Na2_BnUXexmk9fDSWC&s=10"
+              alt="profile"
+              className="w-32 h-32 rounded-full border-4 border-gray-300 object-cover"
+            />
+        </label>
+    
+        <input type='text' placeholder='Enter Name' className=' bg-white border w-full p-2 my-4 mb-2 rounded-lg'/>
+        <input type='email' placeholder='Enter Email' className=' bg-white border w-full p-2 my-4 mb-2 rounded-lg'/>
+        <input type='password' placeholder='Enter Password' className=' bg-white border w-full p-2 my-4 mb-2 rounded-lg'/>
+        <input type='text' placeholder='Enter Bio' className=' bg-white border w-full p-2 my-4 mb-2 rounded-lg'/>
+            <div className='flex justify-between'>
+              <button className='px-3 py-2 bg-green-800 text-white rounded-lg'>Update</button>
+              <button className='px-3 py-2 bg-red-800 text-white rounded-lg'>Cancel</button>
+            </div>
+        </div>
       </div>
-    </div>
+    </div> 
+    </>
   );
 }
 
