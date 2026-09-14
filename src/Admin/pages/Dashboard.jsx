250d41d90916b203a76d008470c8b36ba75649e8
nivakclt <nivakclt@gmail.com>
2026-09-14 17:08:14 +0530

    frontendfinished

--- src/Admin/pages/Dashboard.jsx
+++ src/Admin/pages/Dashboard.jsx
@@ -1,24 +1,43 @@
-import AdminHeader from "./components/AdminHeader"
-import Footer from "../../User/pages/components/Footer"
-import AdminSidebar from "./components/AdminSidebar"
+import AdminHeader from "./components/AdminHeader";
+import Footer from "../../User/pages/components/Footer";
+import AdminSidebar from "./components/AdminSidebar";
+import { FaBook,FaUserCheck,FaUser } from "react-icons/fa";
 function Dashboard() {
   return (
     <>
-    <AdminHeader/>
-    {/* Dashboard */}
-    <div className="w-full">
-      <div className="w-full h-full grid sm:grid-cols-1 md:grid-cols-12">
-        <div className="md:col-span-3">
-          <AdminSidebar/>
-        </div>
-        <div className="md:col-span-9">
-          <h1 className="text-3xl p-4">Dashboard</h1>
+      <AdminHeader />
+      {/* Dashboard */}
+      <div className="w-full">
+        <div className="w-full h-full grid sm:grid-cols-1 md:grid-cols-12">
+          <div className="md:col-span-3">
+            <AdminSidebar />
+          </div>
+          <div className="md:col-span-9">
+            <div className="grid md:grid-cols-3 gap-2 m-2">
+              <div className="bg-violet-600 text-white p-10 rounded-lg shadow-lg">
+                <FaBook/>
+                <h3 className="text-lg font-medium">Total Books</h3>
+                <p className="text-3xl font-bold mt-2">100+</p>
+                              </div>
+
+              <div className="bg-yellow-500 text-white p-10 rounded-lg shadow-lg">
+                 <FaUserCheck/>
+                <h3 className="text-lg font-medium">Total Users</h3>
+                <p className="text-3xl font-bold mt-2">100+</p>
+              </div>
+
+              <div className="bg-green-500 text-white p-10 rounded-lg shadow-lg">
+                <FaUser/>
+                <h3 className="text-lg font-medium">Total Employees</h3>
+                <p className="text-3xl font-bold mt-2">100+</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/Admin/pages/Settings.jsx
+++ src/Admin/pages/Settings.jsx
@@ -2,18 +2,67 @@ import AdminHeader from "./components/AdminHeader"
 import AdminSidebar from "./components/AdminSidebar"
 import Footer from "../../User/pages/components/Footer"
 
+import { FaRegEdit } from "react-icons/fa"
+
 function Settings() {
   return (
     <>
     <AdminHeader/>
     {/* Dashboard */}
     <div className="w-full">
-      <div className="w-full h-full grid sm:grid-cols-1 md:grid-cols-12">
+      <div className="w-full grid sm:grid-cols-1 md:grid-cols-12">
         <div className="md:col-span-3">
           <AdminSidebar/>
         </div>
         <div className="md:col-span-9">
-          <h1 className="text-3xl p-4">Settings</h1>
+          <h1 className="text-3xl p-4 my-3 text-center">Admin Settings</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div>
+              <p  className="text-justify p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eaque consectetur magnam non ipsa consequuntur fugit. Quos laborum, at perferendis quis amet fugit, id alias labore velit eveniet, nisi omnis.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit minus ducimus soluta obcaecati qui nihil similique. Architecto voluptates doloremque consectetur mollitia esse quas, quaerat, possimus quos atque eligendi sed sit?
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo temporibus soluta vero dignissimos ullam maiores aliquam, possimus commodi exercitationem esse quasi sint dolores unde corporis. Dignissimos maxime ipsum harum iure?
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam ipsum doloribus ullam enim minus in! Est, molestias saepe. Velit perspiciatis nihil quo odit alias officiis odio dolorum, possimus temporibus repellat?
+              </p>
+            </div>
+
+            <div className="h-full bg-gray-400 p-4 mx-2">
+            <div className="flex flex-col items-center">
+             <label htmlFor="profpic" className="relative">
+           <input type="file" id="profpic" className="hidden" />
+
+            <img src="https://cdn-icons-png.flaticon.com/512/12225/12225935.png" alt="logo"
+                    className="h-62.5"/>
+            <div className="absolute p-3 bg-yellow-400 text-white bottom-3 right-8 rounded">
+        <FaRegEdit />
+      </div>
+    </label>
+  
+      <input
+        type="text"
+        placeholder="Username"
+        className="w-full bg-white border my-3 p-2 rounded"/>
+      <input
+        type="password"
+        placeholder="Password"
+        className="w-full bg-white border my-3 p-2 rounded"/>
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        className="w-full bg-white border my-3 p-2 rounded"/>
+
+      <div className="grid grid-cols-2 gap-3">
+    <button className=" bg-blue-600 text-white p-3 rounded hover:bg-blue-700">
+      RESET
+    </button>
+
+    <button className=" bg-red-600 text-white p-3 rounded hover:bg-red-700">
+      UPDATE
+    </button>
+  </div>
+
+  </div>
+</div>
+          </div>
         </div>
       </div>
     </div>
